cdb8032e0950ce4b4c4131f08afcc385b7e462f3
Jonah Ringdahl <JonahRingdahl@outlook.com>
2025-06-17 07:12:42 -0400

    remove auth

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using CompanyScheduler;
+using CompanySchedule;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,12 +8,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddOidcAuthentication(options =>
-{
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
-});
-
 await builder.Build().RunAsync();
-//  put into imports  <!--@attribute [Microsoft.AspNetCore.Authorization.Authorize] -->
