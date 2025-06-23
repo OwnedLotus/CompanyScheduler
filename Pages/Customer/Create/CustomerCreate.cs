3f9b7d725ffe914606048fe899f25288e021f2b7
Jonah Ringdahl <jonahringdahl@outlook.com>
2025-06-22 21:16:18 -0400

    Add saving to customer creation / update

--- Pages/Customer/Create/CustomerCreate.cs
+++ Pages/Customer/Create/CustomerCreate.cs
@@ -20,20 +20,64 @@ public partial class CustomerCreateForm : Form
     private void AddCustomerAddButton_Click(object sender, EventArgs e)
     {
         var customerName = addCustomerNameTextBox.Text;
-        var customerPhoneNumber = addAddressPhoneTextBox.Text;
+        var customerAddress1 = addAddress1TextBox.Text;
+        var customerAddress2 = addAddress2TextBox.Text;
+        var customerPostal = addAddressPostalCodeTextBox.Text;
+        var customerCityName = addCityNameTextBox.Text;
+        var customerCountryName = addCountryNameTextBox.Text;
+        var customerPhone = addAddressPhoneTextBox.Text;
 
-        Customer.Address = new Address();
-
-
-
-        string[] inputs = [customerName, customerPhoneNumber];
+        string[] inputs = [
+                customerName,
+                customerAddress1,
+                customerAddress2,
+                customerPostal,
+                customerCityName,
+                customerCountryName
+            ];
 
         if (
-            Address.CheckTextBoxes(inputs) ||
-            Address.OnlyDigitsAndDashes(customerPhoneNumber)
+            Appointment.CheckTextBoxes(inputs) ||
+            Address.OnlyDigitsAndDashes(customerPhone)
             )
         {
+            var country = new Country()
+            {
+                CountryName = customerCountryName,
+                CreateDate = DateTimeOffset.UtcNow,
+                CreatedBy = User.UserName,
+                LastUpdate = Appointment.UpdateFormat(),
+                LastUpdateBy = User.UserName
+            };
+            var city = new City()
+            {
+                CityName = customerCityName,
+                Country = country,
+                CreateDate = DateTimeOffset.UtcNow,
+                CreatedBy = User.UserName,
+                LastUpdate = Appointment.UpdateFormat(),
+                LastUpdateBy = User.UserName
+            };
+            var address = new Address()
+            {
+                Address1 = customerAddress1,
+                Address2 = customerAddress2,
+                City = city,
+                PostalCode = customerPostal,
+                Phone = customerPhone,
+                CreateDate = DateTimeOffset.UtcNow,
+                CreatedBy = User.UserName,
+                LastUpdate = Appointment.UpdateFormat(),
+                LastUpdateBy = User.UserName,
+            };
+
             Customer.CustomerName = customerName;
+            Customer.Address = address;
+            Customer.Active = 0;
+            Customer.CreatedDate = DateTime.UtcNow;
+            Customer.LastUpdate = Appointment.UpdateFormat();
+            Customer.LastUpdateBy = User.UserName;
+
             CustomerCreated?.Invoke(this, Customer);
             _mainForm.Show();
             Close();
--- Pages/Customer/Update/CustomerUpdate.cs
+++ Pages/Customer/Update/CustomerUpdate.cs
@@ -5,15 +5,15 @@ namespace CompanyScheduler.Pages.Customers;
 public partial class CustomerUpdateForm : Form
 {
     public event EventHandler<Customer>? CustomerUpdated;
-    private Customer _customer;
-    private readonly User _user;
+    public Customer Customer { get; private set; }
+    public User User { get; private set; }
     private readonly Form _mainForm;
     public CustomerUpdateForm(User user, Customer customer, Form mainForm)
     {
         InitializeComponent();
-        _user = user;
+        User = user;
         _mainForm = mainForm;
-        _customer = customer;
+        Customer = customer;
 
         updateCustomerNameTextBox.Text = customer.CustomerName;
         updateAddress1TextBox.Text = customer.Address?.Address1;
@@ -28,8 +28,71 @@ public partial class CustomerUpdateForm : Form
 
     private void UpdateCustomerAddButton_Click(object sender, EventArgs e)
     {
-        throw new NotImplementedException();
+        var customerName = updateCustomerNameTextBox.Text;
+        var customerAddress1 = updateAddress1TextBox.Text;
+        var customerAddress2 = updateAddress2TextBox.Text;
+        var customerPostal = updateAddressPostalCodeTextBox.Text;
+        var customerCityName = updateCityNameTextBox.Text;
+        var customerCountryName = updateCountryNameTextBox.Text;
+        var customerPhone = updateAddressPhoneTextBox.Text;
+
+        string[] inputs = [
+                customerName,
+                customerAddress1,
+                customerAddress2,
+                customerPostal,
+                customerCityName,
+                customerCountryName
+            ];
+
+        if (
+            Appointment.CheckTextBoxes(inputs) ||
+            Address.OnlyDigitsAndDashes(customerPhone)
+            )
+        {
+            var country = new Country()
+            {
+                CountryName = customerCountryName,
+                CreateDate = DateTimeOffset.UtcNow,
+                CreatedBy = User.UserName,
+                LastUpdate = Appointment.UpdateFormat(),
+                LastUpdateBy = User.UserName
+            };
+            var city = new City()
+            {
+                CityName = customerCityName,
+                Country = country,
+                CreateDate = DateTimeOffset.UtcNow,
+                CreatedBy = User.UserName,
+                LastUpdate = Appointment.UpdateFormat(),
+                LastUpdateBy = User.UserName
+            };
+            var address = new Address()
+            {
+                Address1 = customerAddress1,
+                Address2 = customerAddress2,
+                City = city,
+                PostalCode = customerPostal,
+                Phone = customerPhone,
+                CreateDate = DateTimeOffset.UtcNow,
+                CreatedBy = User.UserName,
+                LastUpdate = Appointment.UpdateFormat(),
+                LastUpdateBy = User.UserName,
+            };
+
+            Customer.CustomerName = customerName;
+            Customer.Address = address;
+            Customer.Active = 0;
+            Customer.CreatedDate = DateTime.UtcNow;
+            Customer.LastUpdate = Appointment.UpdateFormat();
+            Customer.LastUpdateBy = User.UserName;
+
+            CustomerUpdated?.Invoke(this, Customer);
+            _mainForm.Show();
+            Close();
+        }
     }
+
     private void UpdateCustomerQuitButton_Click(object sender, EventArgs e)
     {
         _mainForm.Show();
