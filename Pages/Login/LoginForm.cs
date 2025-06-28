ff63cd5c8595a1053ac1789c11df1d71d5ec3c88
Jonah Ringdahl <jonahringdahl@outlook.com>
2025-06-27 22:34:48 -0400

    breakout authentication to seperate function

--- Pages/Login/LoginForm.cs
+++ Pages/Login/LoginForm.cs
@@ -13,72 +13,53 @@ namespace CompanyScheduler.Pages.Login;
 public partial class LoginForm : Form
 {
     static readonly string filePath = "Login_History.txt";
-    private string welcomeEN = "Please Login!";
-    private string welcomeJP = "ログインして下さい";
-    private string locEn = "Current Location: ";
-    private string locJp = "現在地:";
-    private string loginEn = "Login: ";
-    private string loginJp = "ログイン";
-    private string passEn = "Password: ";
-    private string passJp = "パスワード";
-    private string quitEn = "Quit";
+    private readonly string welcomeEN = "Please Login!";
+    private readonly string welcomeJP = "ログインして下さい";
+    private readonly string locEn = "Current Location: ";
+    private readonly string locJp = "現在地:";
+    private readonly string loginEn = "Login: ";
+    private readonly string loginJp = "ログイン";
+    private readonly string passEn = "Password: ";
+    private readonly string passJp = "パスワード";
+    private readonly string quitEn = "Quit";
 
-    private string quitJp = "終了";
-    private string loginBtEn = "Login";
-    private string loginBtJp = "入て";
+    private readonly string quitJp = "終了";
+    private readonly string loginBtEn = "Login";
+    private readonly string loginBtJp = "入て";
 
-    private string messageFailedEn = "Username or Password do not match";
-    private string messageFailedJp = "ハンドルやパスワードは不正確です";
+    private readonly string messageFailedEn = "Username or Password do not match";
+    private readonly string messageFailedJp = "ハンドルやパスワードは不正確です";
 
-    private string captionFailedEn = "Failed Login";
-    private string captionFailedJp = "ログイン失敗";
+    private readonly string captionFailedEn = "Failed Login";
+    private readonly string captionFailedJp = "ログイン失敗";
 
     int selection = 0;
 
-    private User _user = new();
     public string LabelText { get; private set; } = RegionInfo.CurrentRegion.DisplayName;
 
     public LoginForm() => InitializeComponent();
 
     private void LoginButton_Clicked(object sender, EventArgs e)
     {
-#if true
         // Check if user is registered
-        using (var context = new ClientScheduleContext())
+        using var context = new ClientScheduleContext();
+        var users = context.Users.AsParallel();
+
+        foreach (var user in users)
         {
-            var users = context.Users.AsParallel();
-
-            foreach (var user in users)
-            {
-                if (userBox.Text == user.UserName && passBox.Text == user.Password && user is not null)
-                {
-                    // Enter the application
-                    UpdateLoginLog(userBox.Text);
-
-                    var home = new HomeForm(user);
-                    home.Show();
-                    Hide();
-                }
-                else
-                {
-                    switch (selection)
-                    {
-                        case 0:
-                            MessageBox.Show(messageFailedEn, captionFailedEn, MessageBoxButtons.OK);
-                            break;
-                        case 1:
-                            MessageBox.Show(messageFailedJp, captionFailedJp, MessageBoxButtons.OK);
-                            break;
-                    }
-                }
-            }
+            if (user is not null)
+                AuthenticateUser(user);
         }
-#else
-        if (userBox.Text == "test" && passBox.Text == "test")
+    }
+
+    private void AuthenticateUser(User user)
+    {
+        if (userBox.Text == user.UserName && passBox.Text == user.Password)
         {
             // Enter the application
             UpdateLoginLog(userBox.Text);
-            var home = new HomeForm(new User());
+
+            var home = new HomeForm(user);
             home.Show();
             Hide();
         }
@@ -94,7 +75,6 @@ public partial class LoginForm : Form
                     break;
             }
         }
-#endif
     }
 
     public void LanguagesSelector_SelectionChanged(object sender, EventArgs e)
@@ -128,10 +108,8 @@ public partial class LoginForm : Form
 
     public void UpdateLoginLog(string username)
     {
-        using (StreamWriter sw = new(filePath, append: true))
-        {
-            sw.WriteLine($"{DateTime.UtcNow}: {userBox.Text}");
-        }
+        using StreamWriter sw = new(filePath, append: true);
+        sw.WriteLine($"{DateTime.UtcNow}: {userBox.Text}");
     }
 
     private void QuitButton_Clicked(object sender, EventArgs e) => Environment.Exit(0);
